d56e7446e1d9b7df4c92dfc4651e645894586618
chetsadaphon wongwiwong <114697178+kihwchetsada@users.noreply.github.com>
2025-09-16 02:05:23 +0700

    Update db.php
    ไม่ได้ใช้

--- db.php
+++ db.php
@@ -1,14 +1,1 @@
-<?php
-$host = "localhost";         
-$dbname = "test_login";  
-$username = "root";          
-$password = "";              
 
-try {
-    $userDb = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    // ตั้งค่าให้ PDO แจ้ง error แบบ exception
-    $userDb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("การเชื่อมต่อล้มเหลว: " . $e->getMessage());
-}
-?>
